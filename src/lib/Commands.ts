ed45a545721d770b41a1eb415d3e631dccec3243
idinium96 <47635037+idinium96@users.noreply.github.com>
2021-06-06 20:33:36 +0800

    ✅ add reactions

--- src/lib/Commands.ts
+++ src/lib/Commands.ts
@@ -1,5 +1,5 @@
 import { options } from '../app';
-import { Message } from 'discord.js';
+import { Message, MessageReaction } from 'discord.js';
 const channels = JSON.parse(process.env.CHANNEL_IDS) as string[];
 
 function commandParser(message: string): [string, string] {
@@ -87,19 +87,21 @@ export default class Commands {
     }
 
     //message should be type of Message from discord but that way typescript throws error on line 8
-    public async handleMessage(message: Message): Promise<Message> {
+    public async handleMessage(message: Message): Promise<Message | MessageReaction> {
         //check for auto-response and if found dont continue
         if (channels.includes(message.channel.id)) {
             const messageOptions = options.getOption(message.content);
             if (messageOptions[1]) {
                 const spam = this.checkSpam(messageOptions[0], message.author.id);
                 if (spam === 'bruh') {
-                    message.react('🤬');
-                    return;
+                    return message.react('🤬');
                 }
 
-                const reply = spam || messageOptions[1];
-                return message.reply(reply as string);
+                if (spam === null) {
+                    message.react('👍');
+                }
+
+                return message.reply((spam || messageOptions[1]) as string);
             }
         }
 
@@ -117,13 +119,16 @@ export default class Commands {
         const isOwner = message.guild.ownerID === message.author.id;
         if (!message.member.roles.cache.find(r => r.id == roleID) && !isOwner) {
             if (command === 'list') {
-                const reply = this.checkSpam(command, message.author.id);
-                if (reply === 'bruh') {
-                    message.react('🤬');
-                    return;
+                const checkSpam = this.checkSpam(command, message.author.id);
+                if (checkSpam === 'bruh') {
+                    return message.react('🤬');
                 }
 
-                return message.reply(reply || options.getList());
+                if (checkSpam === null) {
+                    message.react('👍');
+                }
+
+                return message.reply(checkSpam || options.getList());
             }
 
             return;
@@ -131,123 +136,189 @@ export default class Commands {
 
         //check if its on correct channel
         if (!channels.includes(message.channel.id) && channels.length != 0) {
-            return message.reply(
-                `Any commands should be run on ${channels.map(channel => `<#${channel}>`).join(', ')}`
-            );
+            return message
+                .reply(`Any commands should be run on ${channels.map(channel => `<#${channel}>`).join(', ')}`)
+                .then(message => message.react('❌'));
         }
+
         if (command === 'prefix' && isOwner) {
             //check for missing arguments cause people dumb
             if (args.length < 1) {
-                return message.reply(`Correct Usage: ${prefix}prefix newPrefix.`);
+                return message.reply(`Correct Usage: ${prefix}prefix newPrefix.`).then(message => message.react('✋'));
             }
-            let newPrefix = args.shift();
+
+            const newPrefix = args.shift();
             options.handleBaseOptionOrAlias('prefix', newPrefix);
-            return message.reply(`Changed prefix from ${prefix} to ${newPrefix}`);
+            return message.reply(`Changed prefix from ${prefix} to ${newPrefix}`).then(message => message.react('✅'));
         } else if (command === 'setRole' && isOwner) {
             if (args.length < 1) {
-                return message.reply(`Correct Usage: ${prefix}setRole roleID.`);
+                return message.reply(`Correct Usage: ${prefix}setRole roleID.`).then(message => message.react('✋'));
             }
-            let newRole = args.shift();
+
+            const newRole = args.shift();
             options.handleBaseOptionOrAlias('roleID', newRole);
-            return message.reply(`Changed roleID to ${newRole}`);
+            return message.reply(`Changed roleID to ${newRole}`).then(message => message.react('✅'));
         } else if (command === 'add') {
             if (args.length < 2 && !message.attachments.first()) {
-                return message.reply(`Correct Usage: ${prefix}add command response.`);
+                return message
+                    .reply(`Correct Usage: ${prefix}add command response.`)
+                    .then(message => message.react('✋'));
             }
+
             try {
                 const [devCommand, devResponse] = commandParser(message.content);
-                if (['prefix', 'roleID'].includes(devCommand))
-                    return message.reply(`Can not add base value ${devCommand} as a command.`);
-                if (options.getOption(devCommand)[1] != undefined) {
-                    return message.reply(`Auto-reply for ${'`' + devCommand + '`'} already exists.`);
+                if (['prefix', 'roleID'].includes(devCommand)) {
+                    return message
+                        .reply(`Can not add base value ${devCommand} as a command.`)
+                        .then(message => message.react('❌'));
                 }
+
+                if (options.getOption(devCommand)[1] != undefined) {
+                    return message
+                        .reply(`Auto-reply for ${'`' + devCommand + '`'} already exists.`)
+                        .then(message => message.react('❌'));
+                }
+
                 options.handleOption(devCommand, devResponse, message.attachments.array());
-                return message.channel.send(
-                    `Added auto-reply: ${'`' + devCommand + '`'}, ${
-                        devResponse ? 'with the response: \n> ' + devResponse : 'with the attachment: \n>'
-                    }.`,
-                    { files: message.attachments.array() }
-                );
+                return message.channel
+                    .send(
+                        `Added auto-reply: ${'`' + devCommand + '`'}, ${
+                            devResponse ? 'with the response: \n> ' + devResponse : 'with the attachment: \n>'
+                        }.`,
+                        { files: message.attachments.array() }
+                    )
+                    .then(message => message.react('✅'));
             } catch (err) {
-                return message.reply(err);
+                return message.reply(err).then(message => message.react('❌'));
             }
         } else if (command === 'remove') {
             //check for missing arguments cause people dumb
             if (args.length < 1) {
-                return message.reply(`Correct Usage: ${prefix}remove command.`);
+                return message.reply(`Correct Usage: ${prefix}remove command.`).then(message => message.react('✋'));
             }
-            let delCommand = args.filter(i => i).join(' ');
-            if (['prefix', 'roleID'].includes(delCommand))
+
+            const delCommand = args.filter(i => i).join(' ');
+            if (['prefix', 'roleID'].includes(delCommand)) {
                 return message.reply(`Can not remove base value ${delCommand}.`);
+            }
+
             const isAlias = typeof options.getOption(delCommand, true)[1] === 'string' ? 'alias' : '';
             options.deleteCommand(delCommand);
-            return message.reply(`Deleted auto-reply for ${isAlias} ${'`' + delCommand + '`'}`);
+            return message
+                .reply(`Deleted auto-reply for ${isAlias} ${'`' + delCommand + '`'}`)
+                .then(message => message.react('🚮'));
         } else if (command === 'edit') {
             //check for missing arguments cause people dumb
             if (args.length < 2 && !message.attachments.first()) {
-                return message.reply(`Correct Usage: ${prefix}edit command newResponse.`);
+                return message
+                    .reply(`Correct Usage: ${prefix}edit command newResponse.`)
+                    .then(message => message.react('✋'));
             }
+
             try {
                 const [devCommand, devResponse] = commandParser(message.content);
-                if (['prefix', 'roleID'].includes(devCommand))
-                    return message.reply(`Can not add base value ${devCommand} as a command.`);
+                if (['prefix', 'roleID'].includes(devCommand)) {
+                    return message
+                        .reply(`Can not add base value ${devCommand} as a command.`)
+                        .then(message => message.react('❌'));
+                }
+
                 if (options.getOption(devCommand)[1] != undefined) {
                     options.handleOption(devCommand, devResponse, message.attachments.array());
-                    return message.channel.send(
-                        `Edited auto-reply: ${'`' + devCommand + '`'}, ${
-                            devResponse ? 'with the response: \n> ' + devResponse : 'with the attachment: \n>'
-                        }.`,
-                        { files: message.attachments.array() }
-                    );
+                    return message.channel
+                        .send(
+                            `Edited auto-reply: ${'`' + devCommand + '`'}, ${
+                                devResponse ? 'with the response: \n> ' + devResponse : 'with the attachment: \n>'
+                            }.`,
+                            { files: message.attachments.array() }
+                        )
+                        .then(message => message.react('✅'));
                 }
-                return message.reply(`Auto-reply for ${'`' + devCommand + '`'} doesn't exist.`);
+
+                return message
+                    .reply(`Auto-reply for ${'`' + devCommand + '`'} doesn't exist.`)
+                    .then(message => message.react('❌'));
             } catch (err) {
-                return message.reply(err);
+                return message.reply(err).then(message => message.react('❌'));
             }
         } else if (command === 'list') {
-            return message.reply(options.getList());
+            return message.reply(options.getList()).then(message => message.react('✅'));
         } else if (command === 'alias') {
             if (args.length < 2) {
-                return message.reply(`Correct Usage: ${prefix}alias !help help.\nor: "not found" file not found`);
+                return message
+                    .reply(`Correct Usage: ${prefix}alias !help help.\nor: "not found" file not found`)
+                    .then(message => message.react('✋'));
             }
             try {
                 const [devAlias, devExistingCMD] = commandParser(message.content);
-                if (['prefix', 'roleID'].includes(devAlias))
-                    return message.reply(`Can not alias base value ${devAlias} as a command.`);
-                if (['prefix', 'roleID'].includes(devExistingCMD))
-                    return message.reply(`Can not alias base value ${devExistingCMD} as a target.`);
-                if (options.getOption(devExistingCMD)[1] === undefined)
-                    return message.reply(`Can not target alias for ${'`' + devExistingCMD + '`'} it doesn't exist.`);
-                if (options.getOption(devAlias)[1] !== undefined)
-                    return message.reply(`Can not alias ${devAlias} as it already exists remove it first.`);
+                if (['prefix', 'roleID'].includes(devAlias)) {
+                    return message
+                        .reply(`Can not alias base value ${devAlias} as a command.`)
+                        .then(message => message.react('❌'));
+                }
+
+                if (['prefix', 'roleID'].includes(devExistingCMD)) {
+                    return message
+                        .reply(`Can not alias base value ${devExistingCMD} as a target.`)
+                        .then(message => message.react('❌'));
+                }
+
+                if (options.getOption(devExistingCMD)[1] === undefined) {
+                    return message
+                        .reply(`Can not target alias for ${'`' + devExistingCMD + '`'} it doesn't exist.`)
+                        .then(message => message.react('❌'));
+                }
+
+                if (options.getOption(devAlias)[1] !== undefined) {
+                    return message
+                        .reply(`Can not alias ${devAlias} as it already exists remove it first.`)
+                        .then(message => message.react('❌'));
+                }
 
                 options.handleBaseOptionOrAlias(devAlias, options.getOption(devExistingCMD)[0]);
-                return message.channel.send(`Added alias ${devAlias} => ${options.getOption(devExistingCMD)[0]}`);
+                return message.channel
+                    .send(`Added alias ${devAlias} => ${options.getOption(devExistingCMD)[0]}`)
+                    .then(message => message.react('✅'));
             } catch (err) {
-                return message.reply(err);
+                return message.reply(err).then(message => message.react('❌'));
             }
         } else if (command === 'rename') {
             if (args.length < 2) {
-                return message.reply(
-                    `Correct Usage: ${prefix}rename !help help.\nor: ${prefix}rename "not found" file not found`
-                );
+                return message
+                    .reply(`Correct Usage: ${prefix}rename !help help.\nor: ${prefix}rename "not found" file not found`)
+                    .then(message => message.react('✋'));
             }
+
             try {
                 const [devCurrent, devRename] = commandParser(message.content);
-                if (['prefix', 'roleID'].includes(devCurrent))
-                    return message.reply(`Can not rename base value ${'`' + devCurrent + '`'}.`);
-                if (['prefix', 'roleID'].includes(devRename))
-                    return message.reply(`Can not rename to base value ${'`' + devRename + '`'}.`);
-                if (options.getOption(devCurrent)[1] === undefined)
-                    return message.reply(`Can not rename ${'`' + devCurrent + '`'} it doesn't exist.`);
-                if (options.getOption(devRename)[1] !== undefined)
-                    return message.reply(`Can not rename to ${'`' + devRename + '`'} as it already exists.`);
+                if (['prefix', 'roleID'].includes(devCurrent)) {
+                    return message
+                        .reply(`Can not rename base value ${'`' + devCurrent + '`'}.`)
+                        .then(message => message.react('❌'));
+                }
+                if (['prefix', 'roleID'].includes(devRename)) {
+                    return message
+                        .reply(`Can not rename to base value ${'`' + devRename + '`'}.`)
+                        .then(message => message.react('❌'));
+                }
+                if (options.getOption(devCurrent)[1] === undefined) {
+                    return message
+                        .reply(`Can not rename ${'`' + devCurrent + '`'} it doesn't exist.`)
+                        .then(message => message.react('❌'));
+                }
+                if (options.getOption(devRename)[1] !== undefined) {
+                    return message
+                        .reply(`Can not rename to ${'`' + devRename + '`'} as it already exists.`)
+                        .then(message => message.react('❌'));
+                }
 
                 options.renameCommand(devCurrent, devRename);
 
-                return message.channel.send(`Renamed ${'`' + devCurrent + '`'} => ${'`' + devRename + '`'}`);
+                return message.channel
+                    .send(`Renamed ${'`' + devCurrent + '`'} => ${'`' + devRename + '`'}`)
+                    .then(message => message.react('✅'));
             } catch (err) {
-                return message.reply(err);
+                return message.reply(err).then(message => message.react('❌'));
             }
         }
     }
